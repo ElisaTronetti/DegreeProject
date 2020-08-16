57b0189d9e4683b25edd347ec7c669152be064d7
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2020-08-16 19:05:10 +0200

    Added enum for weather conditions

---
+++ DegreeApp/app/src/main/java/com/example/degreeapp/Utilities/WeatherCondition.java
@@ -0,0 +1,20 @@
+package com.example.degreeapp.Utilities;
+
+/**
+ * Possible weather conditions
+ */
+public enum WeatherCondition {
+    GOOD("Good"),
+    MEDIUM("Medium"),
+    BAD("Bad");
+
+    private String name;
+
+    private WeatherCondition(final String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
