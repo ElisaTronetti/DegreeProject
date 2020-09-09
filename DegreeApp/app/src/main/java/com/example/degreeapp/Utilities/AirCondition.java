466eaa631a112ab600365a57fce39e85bc2b1246
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2020-09-09 13:35:56 +0200

    Added a way to get air description

--- DegreeApp/app/src/main/java/com/example/degreeapp/Utilities/AirCondition.java
+++ DegreeApp/app/src/main/java/com/example/degreeapp/Utilities/AirCondition.java
@@ -3,18 +3,27 @@ package com.example.degreeapp.Utilities;
 /**
  * Possible weather conditions
  */
-public enum WeatherCondition {
+public enum AirCondition {
     GOOD("good"),
     MEDIUM("medium"),
     BAD("bad");
 
     private String name;
 
-    private WeatherCondition(final String name){
+    AirCondition(final String name){
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    public static String getDescription(final AirCondition airCondition){
+        switch (airCondition){
+            case GOOD: return "La qualità dell'aria in questa zona è buona!";
+            case MEDIUM: return  "La qualità dell'aria in questa zona non è male";
+            case BAD: return  "La qualità dell'aria in questa zona è pessima";
+        }
+        return null;
     }
 }
